e6a87e04bf7919c590b8cf3a91da7016db5e41e7
Tracy Ngo <tngo1206@gmail.com>
2019-03-19 03:14:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,8 @@
 class Artist
-
   attr_accessor :name
-  
-  def initialize(name)
-    @name = name
-  end
-
 end
 
 
 class Song
-
-attr_accessor :title, :artist
-
+  attr_accessor :title, :artist
 end
